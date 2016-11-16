7b60e18414397a5f5938258d27a2c4d40f5c676d
Ryan Tablada <ryan.tablada@gmail.com>
2016-11-16 15:02:18 -0600

    fix: Seeds now wait to finish

--- database/seeds/Menu.js
+++ database/seeds/Menu.js
@@ -5,21 +5,29 @@ const Factory = use('Factory')
 const categories = [
   {
     name: 'Small Plates',
+    created_at: new Date(),
+    updated_at: new Date(),
   },
   {
     name: 'Main Courses',
+    created_at: new Date(),
+    updated_at: new Date(),
   }
 ];
 
 const menuItems = [
   {
     name: 'Poke',
+    created_at: new Date(),
+    updated_at: new Date(),
     price: 11,
     description: 'Tuna, Sesame Chicharron, Chili Oil, Apple, Raddish',
     menu_category_id: 1,
   },
   {
     name: 'Pork Gyoza',
+    created_at: new Date(),
+    updated_at: new Date(),
     price: 7,
     description: 'Contry Ponzu',
     menu_category_id: 1,
@@ -27,18 +35,24 @@ const menuItems = [
 
   {
     name: 'Super Salad',
+    created_at: new Date(),
+    updated_at: new Date(),
     price: 6,
     description: 'Cabbage, Bacon, Egg, Nori Green Goddess',
     menu_category_id: 2,
   },
   {
     name: 'Buttered Noodles',
+    created_at: new Date(),
+    updated_at: new Date(),
     price: 9,
     description: 'Miso Butter, Pepper, Hand Pulled Noodles',
     menu_category_id: 2,
   },
   {
     name: 'Hot Chicken',
+    created_at: new Date(),
+    updated_at: new Date(),
     price: 12,
     description: 'Fried Chicken Breast, Nashville Spices',
     menu_category_id: 2,
@@ -50,8 +64,8 @@ const Db = use('Database');
 class MenuSeeder {
 
   * run () {
-    Db.table('menu_categories').insert(categories);
-    Db.table('menu_items').insert(menuItems);
+    yield Db.table('menu_categories').insert(categories);
+    yield Db.table('menu_items').insert(menuItems);
   }
 
 }
